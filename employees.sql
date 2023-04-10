d3e77b98ad6cf26ca86045acb6dd08a47b9ccf52
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2023-04-10 11:54:22 +0600

    code

--- employees.sql
+++ employees.sql
@@ -38,10 +38,10 @@ CREATE TABLE IF NOT EXISTS `employees` (
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`id`, `name`, `address`, `salary`) VALUES
-(1, 'Roland Mendel', 'C/ Araquil, 67, Madrid', 5000),
-(2, 'Victoria Ashworth', '35 King George, London', 6500),
-(3, 'Martin Blank', '25, Rue Lauriston, Paris', 8000);
+-- INSERT INTO `employees` (`id`, `name`, `address`, `salary`) VALUES
+-- (1, 'Roland Mendel', 'C/ Araquil, 67, Madrid', 5000),
+-- (2, 'Victoria Ashworth', '35 King George, London', 6500),
+-- (3, 'Martin Blank', '25, Rue Lauriston, Paris', 8000);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
